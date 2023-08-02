799a06076cf2607e56f2ece6f409386f36569af3
valeriamrk <87308393+valeriamrk@users.noreply.github.com>
2023-08-02 17:39:47 +0300

    env update
    env update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import "./App.css";
 import { AuthProvider } from "oidc-react";
-// import "dotenv/config";
-
-console.log(import.meta.env.CLIENT_ID);
 
 function App() {
   return (
     <>
       <AuthProvider
-        authority={import.meta.env.AUTHORITY}
-        clientId={import.meta.env.CLIENT_ID}
-        clientSecret={import.meta.env.CLIENT_SECRET}
-        redirectUri={import.meta.env.REDIRECT_URI}
+        authority={import.meta.env.VITE_AUTHORITY}
+        clientId={import.meta.env.VITE_CLIENT_ID}
+        clientSecret={import.meta.env.VITE_CLIENT_SECRET}
+        redirectUri={import.meta.env.VITE_REDIRECT_URI}
       >
         <div className="card">
           <button>Кнопка 1</button>
