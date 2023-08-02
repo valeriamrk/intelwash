3e8286ce1d14d89fcccae6747afb81a5304aef5e
valeriamrk <87308393+valeriamrk@users.noreply.github.com>
2023-08-02 18:21:26 +0300

    config update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import { AuthProvider } from "oidc-react";
 
+const oidcConfig = {
+  authority: import.meta.env.VITE_AUTHORITY,
+  clientId: import.meta.env.VITE_CLIENT_ID,
+  clientSecret: import.meta.env.VITE_CLIENT_SECRET,
+  redirectUri: import.meta.env.VITE_REDIRECT_URI,
+};
+
 function App() {
   return (
     <>
-      <AuthProvider
-        authority={import.meta.env.VITE_AUTHORITY}
-        clientId={import.meta.env.VITE_CLIENT_ID}
-        clientSecret={import.meta.env.VITE_CLIENT_SECRET}
-        redirectUri={import.meta.env.VITE_REDIRECT_URI}
-      >
+      <AuthProvider {...oidcConfig}>
         <div className="card">
           <button>Кнопка 1</button>
           <button>Кнопка 2</button>
